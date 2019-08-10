f93986b9d3daed05d111aaa3fb59ce4b772a3a75
Niyizibyose <jean21101999@gmail.com>
2019-08-10 19:01:01 +0200

    Full project

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public bool paused;
+
+    void Start()
+    {
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+       
+
+    public void Pause()
+    {
+
+        paused = !paused;
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+
+        else if (!paused)
+            {
+            Time.timeScale = 1;
+            }
+        }
+    }
+       
+    
+
